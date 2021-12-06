f99db7dcb52155db0f69b353a307ded5b1904fda
Romain <romain.valeri@laposte.net>
2021-12-06 08:47:58 +0100

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -1,24 +1,28 @@
-data = open('inputs/input_day6bis.txt').read()
+data = open('inputs/input_day6.txt').read()
 sea = [int(x) for x in data.split(',')]
-future = list()
-days = 80
+days = 256
+census = [0,0,0,0,0,0,0,0,0]
+population = 0
+
+for fish in sea:
+    census[fish] += 1
+    population += 1
 
 def tick():
-    global sea, future
-    sea = [age(x) for x in sea]
-    sea.extend(future)
-    future = list()
-
-def age(fish):
-    global sea, future
-    if fish == 0:
-        future.append(8)
-        return 6
-    else:
-        return (fish - 1)
+    global census, population
+    for group in range(len(census)):
+        if group == 0:
+            babies = census[group]
+        if group < 8:
+            census[group] = census[group + 1]
+        if group == 6:
+            census[6] += babies
+        if group == 8: 
+            census[8] = babies
+    population += babies
 
 for x in range(days):
     tick()
-    print(x)
+    print('Day ' + str(x+1))
 
-print('Finished : ' + str(len(sea)))
+print('Finished : ' + str(population))
