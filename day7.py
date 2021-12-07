b991d236e7c1274d753c797ca2c4d3616d2205a6
Romain Valeri <romvaler@publicisgroupe.net>
2021-12-07 07:30:38 +0100

    Day 7 part 2

--- day7.py
+++ day7.py
@@ -1,13 +1,19 @@
 data = open('inputs/input_day7.txt').read()
 crabs = [int(x) for x in data.split(',')]
-minFuel = 999999999999
+minFuel = 999999999999999
 bestPos = False
+
+def calculate(d):
+    total = 0
+    for n in range(d + 1):
+        total += n
+    return total
 
 for pos in range(min(crabs), max(crabs)):
     fuel = 0
     for crab in range(len(crabs)):
         distance = abs(pos - crabs[crab])
-        fuel += distance
+        fuel += calculate(distance)
     if fuel < minFuel:
         minFuel = fuel
         bestPos = pos
