c8f098cc8d30f7d0c6f8ecc57db5af986c946ac0
Romain Valeri <romvaler@publicisgroupe.net>
2021-12-09 14:09:19 +0100

    Day 9 part 2

--- day9.py
+++ day9.py
@@ -1,19 +1,36 @@
 data = open('inputs/input_day9.txt').read()
 lines = data.split('\n')
-lowPoints = list()
+knownBasins = list()
+newBasin = list()
+basinSizes = list()
 
-def isLow(line, col):
-    global lines
-    x = int(lines[line][col])
-    up = int(lines[line-1][col]) if line > 0 else 9
-    down = int(lines[line+1][col]) if line < (len(lines)-1) else 9
-    left = int(lines[line][col-1]) if col > 0 else 9
-    right = int(lines[line][col+1]) if col < (len(lines[line])-1) else 9
-    return x < up and x < down and x < left and x < right
+def searchBasin(line, col):
+    global lines, newBasin
+    if lines[line][col] == '9':
+        return False
+    if [line, col] in newBasin:
+        return False
+    for basin in knownBasins:
+        if [line, col] in basin:
+            return False
+    newBasin.append([line, col])
+    if (line > 0):
+        searchBasin(line-1, col)
+    if (line < (len(lines)-1)):
+        searchBasin(line+1, col)
+    if (col > 0):
+        searchBasin(line, col-1)
+    if (col < (len(lines[line])-1)):
+        searchBasin(line, col+1)
 
 for line in range(len(lines)):
     for col in range(len(lines[line])):
-        if isLow(line, col):
-            lowPoints.append(int(lines[line][col]))
+        searchBasin(line, col)
+        if len(newBasin) > 0:
+            knownBasins.append(newBasin)
+            basinSizes.append(len(newBasin))
+            newBasin = list()
 
-print(sum(lowPoints) + len(lowPoints))
+basinSizes.sort(reverse=True)
+print(basinSizes[0]*basinSizes[1]*basinSizes[2])
+
