275140fa27de854b30d7783e215384af6acdc079
Romain Valeri <romvaler@publicisgroupe.net>
2021-12-10 10:47:13 +0100

    Day 10 part 2

--- day10.py
+++ day10.py
@@ -1,6 +1,6 @@
 import re
 
-data = open('inputs/input_day10bis.txt').read()
+data = open('inputs/input_day10.txt').read()
 lines = data.split('\n')
 
 outlaws = '(\(\]|\(\}|\(\>|\{\]|\{\)|\{\>|\[\)|\[\}|\[\>|\<\}|\<\)|\<\])'
@@ -20,20 +20,53 @@ def trimLine(line):
 score = 0
 incompletes = list()
 for line in lines:
-  original = line
   line = trimLine(line)
   suspect = re.search(outlaws, line)
   if suspect != None:
     offender = suspect.group()[1]
     score += errorTable[offender]
   else:
-    incompletes.append(original)
+    incompletes.append(line)
 
 print('Part 1 : ' + str(score))
 
 # --- Part 2 ---
 
-'''
+repairValues = {
+  ")": 1,
+  "]": 2,
+  "}": 3,
+  ">": 4,
+}
+matchings = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+  "<": ">",
+}
+
 def completionScore(repair):
+  score = 0
   for char in repair:
-'''
+    score = (score*5) + repairValues[char]
+  return score
+
+def middleScore(scoreList):
+  scoreList.sort()
+  while len(scoreList) > 1:
+    scoreList.pop(-1)
+    scoreList.pop(0)
+  return scoreList[0]
+
+def completeLine(line):
+  completion = ''
+  for char in line:
+    completion = matchings[char] + completion
+  return completion
+
+scores = list()
+for line in incompletes:
+  ending = completeLine(line)
+  scores.append(completionScore(ending))
+
+print('Part 2 : ' + str(middleScore(scores)))
