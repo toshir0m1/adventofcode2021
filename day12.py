78be2d6498d6491e509deb8d9a0180f17fcdc07e
Romain <romain.valeri@laposte.net>
2021-12-12 10:08:17 +0100

    Day 12 part 2

--- day12.py
+++ day12.py
@@ -13,9 +13,20 @@ for link in links:
     else:
       caves[points[1]][points[0]] = True
 
+def hasDoubleSmall(steps):
+  for step in range(len(steps)):
+    if steps[step].islower():
+      others = steps.copy()
+      del others[step]
+      if steps[step] in others:
+        return True
+  return False
+
 def searchCave(path):
   for nextCave in caves[path[-1]]:
-    if nextCave in path and nextCave.islower():
+    if nextCave == 'start':
+      continue
+    if nextCave in path and nextCave.islower() and hasDoubleSmall(path):
       continue
     nextStep = path.copy()
     nextStep.append(nextCave)
